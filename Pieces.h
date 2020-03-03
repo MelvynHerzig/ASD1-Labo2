00e487bc77a39c2ad2e562a6f693a885786fd32b
MelvynHerzig <34660483+MelvynHerzig@users.noreply.github.com>
2020-03-03 16:03:27 +0100

    Comparaison de deux côté

--- Pieces.h
+++ Pieces.h
@@ -12,11 +12,12 @@
 #include <array>
 #include <vector>
 
-enum AttachementType {
-    FILLE_HAUT, FILLE_BAS, DAME_HAUT, DAME_BAS, ARROSOIR_GAUCHE, ARROSOIR_DROIT, GATEAU_GAUCHE, GATEAU_DROIT,  ARROSOIR_INVERSE, NONE };
+enum AttachementType { FILLE_HAUT, FILLE_BAS, DAME_HAUT, DAME_BAS, ARROSOIR_GAUCHE, ARROSOIR_DROIT, GATEAU_GAUCHE, GATEAU_DROIT,  ARROSOIR_INVERSE, NONE };
 
 using Piece = std::array<AttachementType,4>;
 using Pieces = std::vector<Piece>;
+
+
 
 extern const Pieces PIECES;
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <array>
+#include "Pieces.h"
+
+//Vérifie si une image (de gauche à droite ou de haut en bas) match entre deux pièces.
+bool coteComplementaire(const AttachementType& c1, const AttachementType& c2)
+{
+    switch(c1)
+    {
+        case FILLE_HAUT      : return c2 == FILLE_BAS      ;
+        case FILLE_BAS       : return c2 == FILLE_HAUT     ;
+        case DAME_HAUT       : return c2 == DAME_BAS       ;
+        case DAME_BAS        : return c2 == DAME_HAUT      ;
+        case ARROSOIR_GAUCHE : return c2 == ARROSOIR_DROIT ;
+        case ARROSOIR_DROIT  : return c2 == ARROSOIR_GAUCHE;
+        case GATEAU_GAUCHE   : return c2 == GATEAU_DROIT   ;
+        case GATEAU_DROIT    : return c2 == GATEAU_GAUCHE  ;
+        case ARROSOIR_INVERSE: return c2 == NONE           ;
+    }
+
+    return false;
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
