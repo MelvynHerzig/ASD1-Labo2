ce7997230afa5da0e333828779f63d0cf23c95d1
Quentin Forestier <quentin.forestier@heig-vd.ch>
2020-03-07 16:34:23 +0100

    Fini

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 
 #include <array>     //std::array
 #include <algorithm> //std::rotate
-#include <ostream>   //std::ostream, std::cout
+#include <iostream>   //std::ostream, std::cout
 #include "Pieces.h"  //Piece, PIECES, AttachmentType
 
 class PieceOrientee;
@@ -40,7 +40,7 @@ public:
 };
 
 PieceOrientee::PieceOrientee(const Piece& piece , const Orientation& orientation, unsigned id)
-: piece(piece), orientation(orientation), id(id) {}
+        : piece(piece), orientation(orientation), id(id) {}
 
 void PieceOrientee::rotationGauche()
 {
@@ -52,42 +52,87 @@ std::ostream& operator<<(std::ostream& os, const Puzzle& puzzle)
 {
     for(const PieceOrientee& pieceOrientee : puzzle)
     {
-        os << pieceOrientee.id << pieceOrientee.orientation << ' ';
+        os << pieceOrientee.id << (char)('a' + pieceOrientee.orientation) << ' ';
     }
 
     return os << '\n';
 }
 
-void resolution(Puzzle puzzle, unsigned pieceRestante)
+
+//Vérifie si une image (de gauche à droite ou de haut en bas) match entre deux pièces.
+bool coteComplementaire(const AttachementType& c1, const AttachementType& c2)
+{
+    switch(c1)
+    {
+        case FILLE_HAUT      : return c2 == FILLE_BAS      ;
+        case FILLE_BAS       : return c2 == FILLE_HAUT     ;
+        case DAME_HAUT       : return c2 == DAME_BAS       ;
+        case DAME_BAS        : return c2 == DAME_HAUT      ;
+        case ARROSOIR_GAUCHE : return c2 == ARROSOIR_DROIT ;
+        case ARROSOIR_DROIT  : return c2 == ARROSOIR_GAUCHE;
+        case GATEAU_GAUCHE   : return c2 == GATEAU_DROIT   ;
+        case GATEAU_DROIT    : return c2 == GATEAU_GAUCHE  ;
+        case ARROSOIR_INVERSE: return c2 == NONE           ;
+    }
+
+    return false;
+}
+
+bool verifieCorrespondance(const Puzzle& puzzle, size_t posPiece)
+{
+    unsigned x = posPiece % 3;
+    unsigned y = posPiece / 3;
+
+    bool droiteOk = true;
+    bool basOk = true;
+
+    if((x + 1) < 3)
+    {
+        droiteOk = coteComplementaire(puzzle[posPiece].piece[DROITE], puzzle[posPiece+1].piece[GAUCHE]);
+    }
+    if((y + 1) < 3)
+    {
+        basOk = coteComplementaire(puzzle[posPiece].piece[BAS], puzzle[posPiece+3].piece[HAUT]);
+    }
+    return basOk && droiteOk;
+}
+
+void echanger(Puzzle& puzzle, unsigned pos1, unsigned pos2)
+{
+    PieceOrientee tmp = puzzle[pos1];
+    puzzle[pos1] = puzzle[pos2];
+    puzzle[pos2] = tmp;
+}
+
+void resolution(Puzzle& puzzle, unsigned pieceRestante)
 {
     if(pieceRestante == 1)
     {
-        //Do something
+        for(int r = 0; r < 4; ++r)
+        {
+            if(verifieCorrespondance(puzzle, 0))
+            {
+                std::cout << puzzle << std::endl;
+            }
+            puzzle[0].rotationGauche();
+        }
     }
     else
     {
-        for(size_t i = 1; i <= pieceRestante - 1; ++i)
+        for(size_t i = 0; i < pieceRestante; ++i)
         {
-            //Do something
+            echanger(puzzle, i, pieceRestante-1);
+            for(int r = 0; r < 4; ++r)
+            {
+                if(verifieCorrespondance(puzzle, pieceRestante-1))
+                {
+                    resolution(puzzle, pieceRestante-1);
+                }
+                puzzle[pieceRestante-1].rotationGauche();
+            }
+            echanger(puzzle, i, pieceRestante-1);
         }
     }
-    /*Si piece restante == 1
-        Pour chaque rotation de la pièce
-            si correspondance afficherPuzzle
-            sinon return
-
-      Sinon
-        Pour chaque piece de 1 a 8,
-            la placer en dernière position
-            pour chaque orientation (donc 4 appels à rotationGauche)
-                si correspondance avec la/les précédete
-                    appel recursif
-                fin si
-
-            replacer la piece à se position
-        Pour chaque rotation de la derniere pièce
-            appel recursif
-        fin si */
 }
 
 int main()
